48d7a07d752994c3f6e852911bea06a011b9e0f9
aadyamish <163799329+aadyamish@users.noreply.github.com>
2024-09-12 16:01:15 +1000

    up

--- post_experiment/Regression/trialregression.r
+++
@@ -1,8 +0,0 @@
-rm(list=ls())
-wine = read.csv("/Users/User/Documents/GitHub/DesktopDownthrottlingExperiment/post_experiment/Regression/wine.train.csv")
-
-hist(wine$fixed.acidity)
-hist(wine$volatile.acidity)
-hist(wine$citric.acid)
-
-
